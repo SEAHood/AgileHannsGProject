14f8fd4b6caf123d0b916288c117f44c24649c8a
unknown <frasersharp@admin.computing.dundee.ac.uk>
2012-11-12 21:46:08 +0000

    Maybe got menu linking to euan's work!
    Signed-off-by: unknown <frasersharp@admin.computing.dundee.ac.uk>

--- Stocks Information/src/com/stocks/hannsg/StocksInformationActivity.java
+++ Stocks Information/src/com/stocks/hannsg/StocksInformationActivity.java
@@ -33,8 +33,8 @@ public class StocksInformationActivity extends Activity {
             		 *CHANGE THIS CODE FROM "NEXTACTIVITY" TO CALL THE PORTFOLIO TOTAL VALUE ACTIVITY*
             		 *********************************************************************************/
 
-            		/*Intent myIntent = new Intent(StocksInformationActivity.this, NextActivity.class);
-            		StocksInformationActivity.this.startActivity(myIntent);*/
+            		Intent myIntent = new Intent(StocksInformationActivity.this, com.example.my.first.app.MainActivity.class);
+            		StocksInformationActivity.this.startActivity(myIntent);
             		
             	} else if (position == 1) {
             		
