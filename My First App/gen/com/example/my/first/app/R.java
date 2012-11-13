90bf53bc09f5a99466776079f76edc0cae6e305b
Euan Morrison <euan.39times@gmail.com>
2012-11-13 02:39:57 +0000

    Linked with Sam's, but dies on run

--- My First App/gen/com/example/my/first/app/R.java
+++ My First App/gen/com/example/my/first/app/R.java
@@ -16,7 +16,13 @@ public final class R {
         public static final int ic_stat_star=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int last_update=0x7f070000;
+        public static final int menu_settings=0x7f070006;
+        public static final int val_bp=0x7f070001;
+        public static final int val_expn=0x7f070003;
+        public static final int val_hsba=0x7f070002;
+        public static final int val_mks=0x7f070004;
+        public static final int val_sn=0x7f070005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- My First App/src/com/example/my/first/app/MainActivity.java
+++ My First App/src/com/example/my/first/app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.my.first.app;
 
+import com.example.my.first.app.R;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                
+        com.example.test.MainActivity myActivity = new com.example.test.MainActivity();
+        myActivity.getShares();
+        
+        String[] shares = myActivity.showShares();
+        displayAll(shares);
+    }
+    
+    public void displayAll(String[] shares) {
+    	TextView t = (TextView)findViewById(R.id.val_bp);
+    	t.setText(shares[0]);
     }
 
     @Override
